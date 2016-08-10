d496f527c5b612990e1b37bfded57f3ed0b61755
vidacore <vidacore92@gmail.com>
2016-08-09 23:47:11 -0500

    Initial commit

---
+++ app/src/main/java/mx/unam/appgit/VentanaPrincipal.java
@@ -0,0 +1,13 @@
+package mx.unam.appgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VentanaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_principal);
+    }
+}
